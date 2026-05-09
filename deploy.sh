beae82a3541019dd9b207d6a905ea25ce30c099f
Andrew Brampton <github@bramp.net>
2026-05-09 12:41:14 -0700

    chore: Rename master branch to main and update references

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo -e "\033[0;31mDEPRECATED: Manual deployment is no longer required.\033[0m"
-echo -e "\033[0;32mSimply push your changes to the master branch, and GitHub Actions will handle the deployment.\033[0m"
+echo -e "\033[0;32mSimply push your changes to the main branch, and GitHub Actions will handle the deployment.\033[0m"
 echo ""
-echo "Pushing to master..."
+echo "Pushing to main..."
 
-git push origin master
+git push origin main
