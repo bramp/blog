e0602e7d6d421bee168f110bcc1c5b50cb9b9be9
Andrew Brampton <github@bramp.net>
2026-05-09 12:22:02 -0700

    fix: Silence 'command not found' noise in md5ln.sh cross-platform check

--- md5ln.sh
+++ md5ln.sh
@@ -6,9 +6,12 @@ extension="${src#*.}"
 filename="${src%%.*}"
 
 # Cross-platform MD5
+# We use command -v and redirect stderr to avoid "command not found" noise in logs
 if command -v md5 >/dev/null 2>&1; then
-    hash=$(md5q "$src" 2>/dev/null || md5 -q "$src")
+    # macOS
+    hash=$(md5 -q "$src")
 elif command -v md5sum >/dev/null 2>&1; then
+    # Linux
     hash=$(md5sum "$src" | awk '{ print $1 }')
 else
     echo "Error: md5 or md5sum not found"
