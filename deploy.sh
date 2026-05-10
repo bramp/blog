6d0e223ccc7b162bcf044fb6f0c046f079e43f53
Andrew Brampton <github@bramp.net>
2026-05-10 09:32:45 -0700

    chore: Remove deprecated manual deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo -e "\033[0;31mDEPRECATED: Manual deployment is no longer required.\033[0m"
-echo -e "\033[0;32mSimply push your changes to the main branch, and GitHub Actions will handle the deployment.\033[0m"
-echo ""
-echo "Pushing to main..."
-
-git push origin main
